c1820569edde3b51bc411ae9043043e267f5eee0
Sagadat Kuandykov <sgkuandykov@gmail.com>
2022-09-22 22:54:23 +0600

    Initial commit

---
+++ app/src/test/java/saga/io/ui_designes_androidstudio/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package saga.io.ui_designes_androidstudio
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
